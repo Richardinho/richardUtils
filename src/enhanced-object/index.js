450d39d4ff8f8aad9d76035c765937d4ba397800
Richard Hunter <richard@richardhunter.co.uk>
2020-10-22 22:24:46 +0100

    removing unused packages

--- src/enhanced-object/index.js
+++ src/enhanced-object/index.js
@@ -1,182 +1,182 @@
 /* globals Proxy */
 
 (function (root, factory) {
-	'use strict';
+  'use strict';
 
-	if (typeof define === 'function' && define.amd) {
-		define(['../sundry'], factory);
-	} else if (typeof module === 'object' && module.exports) {
-		module.exports = factory(require('../sundry'));
-	} else {
-		// Browser globals (root is window)
-		root.enhancedObject = factory(window.sundry);
-	}
+  if (typeof define === 'function' && define.amd) {
+    define(['../sundry'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory(require('../sundry'));
+  } else {
+    // Browser globals (root is window)
+    root.enhancedObject = factory(window.sundry);
+  }
 }(this, function () {
 
-	'use strict';
+  'use strict';
 
-	if (!window.Proxy || !isFunction(window.Proxy)) {
-		throw new Error("Proxy function not implemented in this environment");
-	}
+  if (!window.Proxy || !isFunction(window.Proxy)) {
+    throw new Error("Proxy function not implemented in this environment");
+  }
 
-	// utility functions
-	function isObject(value) {
-		return Object.prototype.toString.call(value) === "[object Object]";
-	}
+  // utility functions
+  function isObject(value) {
+    return Object.prototype.toString.call(value) === "[object Object]";
+  }
 
-	function isFunction(functionToCheck) {
-		var getType = {};
-		return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
-	}
+  function isFunction(functionToCheck) {
+    var getType = {};
+    return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+  }
 
-	//  mixin utility function
-	function extend(destination, source) {
-		for (var k in source) {
-			if (source.hasOwnProperty(k)) {
-				destination[k] = source[k];
-			}
-		}
-		return destination;
-	}
+  //  mixin utility function
+  function extend(destination, source) {
+    for (var k in source) {
+      if (source.hasOwnProperty(k)) {
+        destination[k] = source[k];
+      }
+    }
+    return destination;
+  }
 
-	var events = {
+  var events = {
 
-		trigger : function (eventName) {
+    trigger : function (eventName) {
 
-			if(!this.listeners || !this.listeners[eventName]) {
-				return;
-			}
+      if(!this.listeners || !this.listeners[eventName]) {
+        return;
+      }
 
-			var args = Array.prototype.slice.call(arguments, 1);
+      var args = Array.prototype.slice.call(arguments, 1);
 
-			this.listeners[eventName].forEach(function (listener) {
+      this.listeners[eventName].forEach(function (listener) {
 
-				listener.action.apply(listener.context, args);
+        listener.action.apply(listener.context, args);
 
-			});
-		},
+      });
+    },
 
-		/**
-		*
-		*
-		*
-		*
-		*/
-		on : function (eventName, action, context) {
+    /**
+     *
+     *
+     *
+     *
+     */
+    on : function (eventName, action, context) {
 
-			if(!this.listeners) {
-				this.listeners = {};
-			}
+      if(!this.listeners) {
+        this.listeners = {};
+      }
 
-			if(!this.listeners[eventName]) {
-				this.listeners[eventName] = [];
-			}
+      if(!this.listeners[eventName]) {
+        this.listeners[eventName] = [];
+      }
 
-			this.listeners[eventName].push({
-				action : action,
-				context : context
-			});
-		},
-		// could pass through the event target, the object the event occurred on
-		broadcast : function () {
+      this.listeners[eventName].push({
+        action : action,
+        context : context
+      });
+    },
+    // could pass through the event target, the object the event occurred on
+    broadcast : function () {
 
-			this.trigger.apply(this, arguments);
+      this.trigger.apply(this, arguments);
 
-			for(var key in this) {
+      for(var key in this) {
 
-				if(key !== 'listeners' && key !== 'parent') {
+        if(key !== 'listeners' && key !== 'parent') {
 
-					var value = this[key];
+          var value = this[key];
 
-					if(isObject(value)) {
+          if(isObject(value)) {
 
-						value.broadcast.apply(value, arguments);
+            value.broadcast.apply(value, arguments);
 
-					}
-				}
-			}
-		},
-
-
-		off : function () {
-			// todo: to implement
-		},
-
-		// could pass through the event target, the object the event occurred on
-		fire : function () {
-
-			this.trigger.apply(this, arguments);
-
-			if (this.parent) {
-				this.parent.fire.apply(this.parent, arguments);
-			}
-
-		}
-	};
-
-	var handler = {
-
-		// intercepts property assignment on an object
-		set : function (target, name, value) {
-
-			if(name === 'parent' || name === 'listeners' || isFunction(value)) {
-				target[name] = value;
-				return true;
-			}
-
-			if(isObject(value)) {
-
-				value.parent = target;
-
-				// proxyupObj will iterate through all the props of the value obj. Since set is called whenever a property is set
-				// if any of the child properties are objects, proxyupObj will be called recursively.
-				target[name] = proxyUpObj(value);
-
-			}
-			//  primitive types are just assigned normally. We fire a change event.
-			else {
-
-				target[name] = value;
-				target.fire("change", value);
-			}
-			return true;
-		}
-	};
+          }
+        }
+      }
+    },
 
 
-	function proxyUpObj(obj) {
+    off : function () {
+      // todo: to implement
+    },
 
-		var proxy = new Proxy({}, handler);
+    // could pass through the event target, the object the event occurred on
+    fire : function () {
 
-		// mixin events
-		extend(proxy, events);
+      this.trigger.apply(this, arguments);
 
-		for(var prop in obj) {
+      if (this.parent) {
+        this.parent.fire.apply(this.parent, arguments);
+      }
 
-			if(obj.hasOwnProperty(prop)) {
+    }
+  };
 
-				proxy[prop] = obj[prop];
+  var handler = {
 
-			}
-		}
-		return proxy;
-	}
+    // intercepts property assignment on an object
+    set : function (target, name, value) {
+
+      if(name === 'parent' || name === 'listeners' || isFunction(value)) {
+        target[name] = value;
+        return true;
+      }
+
+      if(isObject(value)) {
+
+        value.parent = target;
+
+        // proxyupObj will iterate through all the props of the value obj. Since set is called whenever a property is set
+        // if any of the child properties are objects, proxyupObj will be called recursively.
+        target[name] = proxyUpObj(value);
+
+      }
+      //  primitive types are just assigned normally. We fire a change event.
+      else {
+
+        target[name] = value;
+        target.fire("change", value);
+      }
+      return true;
+    }
+  };
 
 
-	return proxyUpObj;
+  function proxyUpObj(obj) {
+
+    var proxy = new Proxy({}, handler);
+
+    // mixin events
+    extend(proxy, events);
+
+    for(var prop in obj) {
+
+      if(obj.hasOwnProperty(prop)) {
+
+        proxy[prop] = obj[prop];
+
+      }
+    }
+    return proxy;
+  }
+
+
+  return proxyUpObj;
 
 }));
 
 /*
-	experimental code to allow the creation of objects which can have event handlers attached to them with bubbling and broadcasting functionality supported.
+  experimental code to allow the creation of objects which can have event handlers attached to them with bubbling and broadcasting functionality supported.
 
 */
 
 /* todo:
-	 4. work into an actual software project to see it working 'live'
-	 5. write up as blog post.
-	 6. add ability to cancel propagation within handlers
-	 7. work in event namespacing
+   4. work into an actual software project to see it working 'live'
+   5. write up as blog post.
+   6. add ability to cancel propagation within handlers
+   7. work in event namespacing
 
 */
 
