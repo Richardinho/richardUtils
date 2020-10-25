19c747a26c6eef3bd67c33ea0d71668d9d52f5a3
Richard Hunter <richard@richardhunter.co.uk>
2020-10-25 20:58:16 +0000

    adding UMD to file

--- src/mvc/utils.js
+++ src/mvc/utils.js
@@ -1,14 +1,31 @@
-function arrayPush(array, value) {
-  return [...array, value];
-}
+(function (root, factory) {
+  'use strict';
 
-function arraySplice(array, index) {
-  return array.filter((_, i) => {
-    return index !== i; 
-  });
-}
+  if (typeof define === 'function' && define.amd) {
+		define([], function () {
+			return factory();
+		});
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.utils = factory();
+  }
+}(this, function () {
 
-exports.arraySplice = arraySplice;
-exports.arrayPush = arrayPush;
+  function arrayPush(array, value) {
+    return [...array, value];
+  }
+
+  function arraySplice(array, index) {
+    return array.filter((_, i) => {
+      return index !== i; 
+    });
+  }
+
+  return {
+    arraySplice: arraySplice,
+    arrayPush: arrayPush,
+  };
+}));
 
 
