7c0da0cfaa2a2fbd86a8942d6dfb98042772d148
Richard Hunter <richard@richardhunter.co.uk>
2021-12-11 20:06:36 +0000

    slight change

--- src/mvc/utils.js
+++ src/mvc/utils.js
@@ -2,9 +2,9 @@
   'use strict';
 
   if (typeof define === 'function' && define.amd) {
-		define([], function () {
-			return factory();
-		});
+    define([], function () {
+      return factory();
+    });
   } else if (typeof module === 'object' && module.exports) {
     module.exports = factory();
   } else {
